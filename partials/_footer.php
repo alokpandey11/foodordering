8ff66d96cce4d1fddbf8bce3edea0e649bc0db0d
ALOK PANDEY <alokpandey1131@gmail.com>
2021-10-09 23:51:30 +0530

    deleted unwanated code

--- partials/_footer.php
+++
@@ -1,3 +0,0 @@
-<div class="footer container-fluid bg-dark text-light">
-    <p class="text-center py-2 mb-0">Copyright © 2021 Designed by <a href="https://github.com/alokpandey11" target="_blank" rel="noopener noreferrer">@alokpandey</a></p>
-</div>
